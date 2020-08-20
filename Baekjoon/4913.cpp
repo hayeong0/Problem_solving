ef5c649184d088f46826938d98ae9883f4460272
chy0428 <21800758@handong.edu>
2020-08-20 15:29:26 +0900

    Modify

--- Baekjoon/4913.cpp
+++ Baekjoon/4913.cpp
@@ -24,7 +24,39 @@ int main() {
             }
         }
      
-     
+        // for(int i = L; i <= sqrt(U); i++){
+        //     if(!isprame[i]) continue;
+        //     for(int j = i+i; j <= U; j+= i){
+        //         isprame[j] = 0;   //소수 아님
+        //         break;
+        //     }
+            
+        // }
+        
+        // for(int i = 2; i <= U; i++) {
+        //     if(isprame[i] != 0) {
+        //         cout << i << '\n';
+        //         x++;
+        //     }
+        //     if(i%4 == 1) {
+        //         cout << "test: " << i << '\n';
+        //         y++;
+        //     }     
+        // }
+        // for(int i = L; i < U+1; i++){
+        //     for(int j = 2; j <= sqrt(i); j++){
+        //         if(i%j == 0) {  //소수X
+        //             isprame = 0;
+        //             break;
+        //         }
+        //     }
+        //     if(isprame){
+        //         x++; //소수
+        //         if(i%4 == 1) y++;
+        //     }
+        //     isprame = 1; //초기화
+        // }
+
         printf("%d %d\n", x, y);
         // x = 0, y = 0;
 	}

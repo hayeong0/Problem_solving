9d52a4d305529cb245cf69720e6ec6a79b89e7cb
chy0428 <21800758@handong.edu>
2021-01-19 20:00:21 +0900

    Fix time complexity

--- Baekjoon/11729.cpp
+++ Baekjoon/11729.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
-vector<pair<int, int> > v;
 
-void hanoi(int n, int x, int y, int z) {
-    if (n == 1) v.push_back(make_pair(x, z));
+void hanoi(int n, int start, int to, int by) {
+    if (n == 1) printf("%d %d\n", start, to);
     else {
-        hanoi(n-1, x, z, y);
-        v.push_back(make_pair(x, z));
-        hanoi(n-1, y, x, z);
+        hanoi(n-1, start, by, to);
+        printf("%d %d\n", start, to);
+        hanoi(n-1, by, to, start);
     }
 }
 
 int main() {
     int n;
-
     cin >> n;
-    hanoi(n, 1, 2, 3);
-    cout << v.size() << endl;
+    cout << (1<<n) -1 << "\n";
 
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i].first << " " << v[i].second << endl;
-    }
+    hanoi(n, 1, 3, 2);
 
     return 0;
 }

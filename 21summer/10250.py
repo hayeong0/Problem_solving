4ad551ad9e06581fd912607050664ab74cd18946
최하영 <hayeong@MacBook-Pro.local>
2021-07-24 23:32:16 +0900

    BOJ 10250.py

---
+++ 21summer/10250.py
@@ -0,0 +1,11 @@
+# BOJ 10250.py
+
+for _ in range(int(input())):
+    h, w, n = map(int, input().split())
+    floor = n%h
+    num = n//h+1
+    if floor == 0:      
+        num = n//h 
+        floor = h
+
+    print(f'{floor*100+num}')
